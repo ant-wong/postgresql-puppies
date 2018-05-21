5660eed7ffd4cb6e31f03816935993df2d9a32a7
Anthony Wong <anthony.wong182@gmail.com>
2018-05-21 15:40:22 -0700

    put/delete & err handler

--- queries.js
+++ queries.js
@@ -57,11 +57,40 @@ createPuppy = (req, res, next) => {
   })
 }
 
+updatePuppy = (req, res, next) => {
+  db.none('update pups set name=$1, breed=$2, age=$3, sex=$4 where id =$5', [req.body.name, req.body.breed, Number(req.body.age), req.body.sex, Number(req.params.id)])
+    .then(() => {
+      res.status(200)
+        .json({
+          status: 'success',
+          message: 'Updated puppy. awe'
+        })
+    })
+    .catch((err) => {
+      return next(err)
+    })
+}
+
+removePuppy = (req, res, next) => {
+  const pupID = parseInt(req.params.id)
+  db.result('delete from pups where id =$1', pupID)
+    .then((result) => {
+      res.status(200)
+        .json({
+          status: 'success',
+          message: `Removed ${result.rowCount} puppy. awe :(`
+        })
+    })
+    .catch((err) => {
+      return next(err)
+    })
+}
+
 module.exports = {
   getAllPuppies: getAllPuppies,
   getSinglePuppy: getSinglePuppy,
   createPuppy: createPuppy,
-  // updatePuppy: updatePuppy,
-  // removePuppy: removePuppy
+  updatePuppy: updatePuppy,
+  removePuppy: removePuppy
 }
 
--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ router.get('/', (req, res, next) => {
 router.get('/puppies', db.getAllPuppies)
 router.get('/puppies/:id', db.getSinglePuppy)
 router.post('/puppies', db.createPuppy)
-// router.put('/puppies/:id', db.updatePuppy)
-// router.delete('/puppies/:id', db.removePuppy)
+router.put('/puppies/:id', db.updatePuppy)
+router.delete('/puppies/:id', db.removePuppy)
 
 module.exports = router
